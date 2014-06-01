e308d642df8dfd247a93be67809e7a3c9165f401
bmistree <bmistree@gmail.com>
2014-05-31 23:55:01 -0700

    Added a transition method for circles.

--- test/test_alternate_sort.js
+++ test/test_alternate_sort.js
@@ -1,7 +1,12 @@
 GRAPH_DIV_ID = 'graph_id';
+CHANGE_SORT_INDEX_BUTTON_DIV_ID = 'change_sort_index_button_id';
 
 WIDTH = 500;
 HEIGHT = 100;
+
+var last_sort_by_index = 'i1';
+var all_circles = null;
+
 
 function on_ready()
 {
@@ -17,7 +22,7 @@ function on_ready()
         attr('width', WIDTH).
         attr('height', HEIGHT);
     
-    drawing_area.selectAll('circle').
+    all_circles = drawing_area.selectAll('circle').
         data(SORT_DATA).
         enter().
         append('circle').
@@ -30,6 +35,35 @@ function on_ready()
         attr('cy',50).
         attr('r',20).
         style('fill','steelblue');
+
+    $('#' + CHANGE_SORT_INDEX_BUTTON_DIV_ID).click(
+        change_sort_index_handler);
+}
+
+function change_sort_index_handler()
+{
+    var field_to_sort_by = 'i2';
+    if (last_sort_by_index == 'i2')
+    {
+        field_to_sort_by = 'i1';
+        last_sort_by_index = 'i1';
+    }
+    else
+        last_sort_by_index = 'i2';
+
+    var min_value = min_field_value(SORT_DATA,field_to_sort_by);
+    var max_value = max_field_value(SORT_DATA,field_to_sort_by);
+
+    var new_x_pos_scale = d3.scale.linear().domain([min_value,max_value]).
+        rangeRound([0,WIDTH]);
+    
+    all_circles.transition().
+        attr('cx',
+             function (datum)
+             {
+                 return new_x_pos_scale(datum[field_to_sort_by]);
+             }).
+        duration(1000);
 }
 
 function min_field_value(data_list,obj_index)
