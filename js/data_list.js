b37f6e17beb04edea548f0c6d25bcf4827c5703d
bmistree <bmistree@gmail.com>
2014-06-19 23:40:28 -0700

    Adding js display library for multiple objects.

---
+++ js/data_list.js
@@ -0,0 +1,32 @@
+CHECKBOX_ID_PREFIX = 'check_box_id_prefix_';
+
+/**
+ @param {list} obj_list --- Each element is an object with numerical
+ fields.
+ 
+ @param {list} obj_fields_list --- Each element is a string of a field
+ that we can display on the object
+ 
+ @param {string} drawing_div_id
+
+ @param {string} checkbox_div_id
+ */
+function (obj_list, obj_fields_list,drawing_div_id,checkbox_div_id)
+{
+    $('#'+checkbox_div_id).html(
+        generate_checkbox_list_html(obj_field_list));
+}
+
+function generate_checkbox_list_html(obj_field_list)
+{
+    var to_return = '';
+
+    for (var i = 0; i < obj_field_list.length; ++i)
+    {
+        var obj_field = obj_field_list[i];
+        to_return += (
+            '<input type="checkbox" id="' + CHECKBOX_ID_PREFIX + i +
+                '">' + obj_field + '<br/>');
+    }
+    return to_return;
+}
