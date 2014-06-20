a6f7a12c00ad548ee40689c4a9d2d633f632b8c5
bmistree <bmistree@gmail.com>
2014-06-19 23:56:12 -0700

    Detecting mouse clicks on checkboxes.

--- js/data_list.js
+++ js/data_list.js
@@ -17,6 +17,30 @@ function draw_data_list (
 {
     $('#'+checkbox_div_id).html(
         generate_checkbox_list_html(obj_fields_list));
+    add_checkbox_listeners(obj_fields_list,obj_list);
+}
+
+function add_checkbox_listeners(obj_fields_list,obj_list)
+{
+    for (var i=0; i < obj_fields_list.length; ++i)
+    {
+        var obj_field = obj_fields_list[i];
+        var checkbox_id = CHECKBOX_ID_PREFIX + i;
+        $('#' + checkbox_id).click(
+            checkbox_listener_factory(obj_field,obj_list));
+    }
+}
+
+/**
+ @returns function
+ */
+function checkbox_listener_factory(obj_field,obj_list)
+{
+    return function()
+    {
+        console.log('Clicked');
+        console.log(this);
+    };
 }
 
 function generate_checkbox_list_html(obj_fields_list)
