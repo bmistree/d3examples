2a2396508ee722a516f8e80a53501e878a72f632
bmistree <bmistree@gmail.com>
2014-09-29 22:35:48 -0700

    Draw data based on difference between two fields.

--- js/compare.js
+++ js/compare.js
@@ -9,6 +9,14 @@ function CompareParams(div_id_to_draw_on)
     this.header_spacing = 80;
 
     this.header_y_offset = 20;
+    
+    this.object_a_y_offset = 20;
+    this.object_b_y_offset = 40;
+
+    this.body_rect_a_y_offset = 5;
+    this.body_rect_b_y_offset = 25;
+    this.cell_height = 20;
+    this.cell_width = 80;
 }
 
 CompareParams.prototype.prepend_div_id = function(to_prepend_to)
@@ -36,7 +44,6 @@ function Compare(
     this.fields_to_compare_list = fields_to_compare_list;
     this.compare_params = compare_params;
 
-
     this.update();
 }
 
@@ -58,15 +65,141 @@ Compare.prototype.update = function ()
     draw_body(this,table_body_div_id);
 };
 
+function PairedObject(value,background_color,belongs_to_a,col_index)
+{
+    this.value = value;
+    this.background_color = background_color;
+    this.belongs_to_a = belongs_to_a;
+    this.col_index = col_index;
+}
+
 /**
  @param {Compare} compare_object
+ @param {string} body_div_id --- The id of the div to draw
+ the table body on.
+ */
+function draw_body(compare_object,body_div_id)
+{
+    var normalized_min_color_scale = d3.scale.linear()
+        .domain([0,1])
+        .range(["red", "white"]);
+    // put the normalized min value into this to get result
+    var normalized_max_color_scale = d3.scale.linear()
+        .domain([0,1])
+        .range(["green", "white"]);
+    
+    
+    // First, convert all object data to PairedObjects,
+    // which then feed into d3 selector.
+    var fields_to_compare_list = compare_object.fields_to_compare_list;
+    var paired_object_list = [];
+    for (var i = 0; i < fields_to_compare_list.length; ++i)
+    {
+        var field = fields_to_compare_list[i];
+        var object_a_value = compare_object.obj_a[field];
+        var object_b_value = compare_object.obj_b[field];
+
+        var max_value = Math.max(object_a_value,object_b_value);
+        var min_value = Math.min(object_a_value,object_b_value);
+        var normalized_min_value = min_value/max_value;
+        
+        // note that we intentionally enter normalized_min_value
+        // to get mirrored color intensity, except for max value.
+        var max_color =
+            normalized_max_color_scale(normalized_min_value);
+        var min_color =
+            normalized_min_color_scale(normalized_min_value);
+
+        var a_color = min_color;
+        var b_color = max_color;
+        if (max_value == object_a_value)
+        {
+            // a is bigger than b.
+            a_color = max_color;
+            b_color = min_color;
+        }
+
+        var a_obj = new PairedObject(object_a_value, a_color,true,i);
+        var b_obj = new PairedObject(object_b_value, b_color,false,i);
+        paired_object_list.push(a_obj);
+        paired_object_list.push(b_obj);
+    }
+
+    // draw paired objects
+    var body = d3.select('#' + body_div_id).
+        append('svg:svg').
+        attr('width', compare_object.compare_params.div_width).
+        attr('height', 200);
+
+
+    var body_fill =  body.selectAll('rect').
+        data(paired_object_list).
+        enter().
+        append('svg:rect').
+        attr('height', compare_object.compare_params.cell_height).
+        attr('width', compare_object.compare_params.cell_width).
+        attr('x',
+             function (datum,i)
+             {
+                 var col_index = datum.col_index;
+                 var to_return = header_x(col_index,compare_object.compare_params);
+                 return to_return;
+             }).
+        attr('y',
+             function (datum, i)
+             {
+                 var to_return = 
+                     compare_object.compare_params.body_rect_a_y_offset;
+                 if (! datum.belongs_to_a)
+                 {
+                     to_return =
+                         compare_object.compare_params.body_rect_b_y_offset;
+                 }
+                 return to_return;
+             }).
+        attr('fill',
+            function(datum)
+            {
+                return datum.background_color;
+            }).
+        style('opacity',1.0);
+
+    var body_texts = body.selectAll('text').
+        data(paired_object_list).
+        enter().
+        append('svg:text').
+            attr('x',
+                 function (datum,i)
+                 {
+                     var col_index = datum.col_index;
+                     return header_x(col_index,compare_object.compare_params);
+                 }).
+            attr('y',
+                 function(datum)
+                 {
+                     if (datum.belongs_to_a)
+                         return compare_object.compare_params.object_a_y_offset;
+                     return compare_object.compare_params.object_b_y_offset;
+                 }).
+            text(
+                function(datum)
+                {
+                    return datum.value;
+                });
+    
+}
+
+/**
+ @param {Compare} compare_object
+ @param {string} column_header_div_id --- The id of the div
+ to draw column headers on.
  */
 function draw_headers(compare_object,column_header_div_id)
 {
     var headers = d3.select('#' + column_header_div_id).
         append('svg:svg').
-        attr('width', this.compare_params.div_width).
-        attr('height', 50);
+        attr('width', compare_object.compare_params.div_width).
+        attr('height', 30);
     
     var headers_texts = headers.selectAll('text').
         data(compare_object.fields_to_compare_list).
@@ -75,7 +208,6 @@ function draw_headers(compare_object,column_header_div_id)
             attr('x',
                  function (datum,i)
                  {
-                     console.log("Got in here " + i);
                      return header_x(i,compare_object.compare_params);
                  }).
            attr('y', compare_object.compare_params.header_y_offset).
